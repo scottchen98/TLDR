d0449ca5813a3f89613c95a5da01e4d74fbbb905
scottchen98 <scott.chen1117@gmail.com>
2024-03-29 14:15:27 -0700

    update url with search params in UrlQuery component

--- src/app/url-query.tsx
+++ src/app/url-query.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import { Link as LinkUrl } from "lucide-react";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 export default function UrlQuery() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [urlQuery, setUrlQuery] = useState("");
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const params = new URLSearchParams(searchParams);
+
+    urlQuery ? params.set("query", urlQuery) : params.delete("query");
+    replace(`${pathname}?${params.toString()}`);
+    setUrlQuery("");
+  }
+
   return (
     <div className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex flex-1 items-start justify-center">
@@ -11,13 +29,15 @@ export default function UrlQuery() {
             Past a URL for a quick summary.
           </h3>
           <div className="w-full flex-1">
-            <form>
+            <form onSubmit={onSubmit}>
               <div className="relative">
                 <LinkUrl className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                   type="search"
                   placeholder="Enter a URL here..."
                   className="w-full appearance-none bg-background pl-8 shadow-none"
+                  value={urlQuery}
+                  onChange={(e) => setUrlQuery(e.target.value)}
                 />
               </div>
             </form>
