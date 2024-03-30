d3766151b48a6fab7645f20debb584ab2eebd05f
scottchen98 <scott.chen1117@gmail.com>
2024-03-30 10:26:31 -0700

    update greeting component height calculation

--- src/app/greeting.tsx
+++ src/app/greeting.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Greeting() {
   return (
-    <div className="flex h-dvh flex-col items-center justify-center">
+    <div className="flex h-[calc(100dvh-61px)] flex-col items-center justify-center">
       <div className="flex flex-col gap-3 text-center text-4xl font-medium md:text-5xl lg:text-6xl">
         <span className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
           Hello, Scott
