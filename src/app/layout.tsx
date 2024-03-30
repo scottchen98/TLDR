f91fbdebfcf5520393cbdcafa9b6c780e13ddf76
scottchen98 <scott.chen1117@gmail.com>
2024-03-30 10:21:55 -0700

    update layout styles and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,30 +24,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-          <div className="hidden border-r bg-muted/40 md:block">
+        <div className="grid min-h-dvh w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <div className="hidden bg-muted/40 md:block">
             <div className="flex h-full max-h-screen flex-col gap-2">
-              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                <Link
-                  href="/"
-                  className="flex items-center gap-2 font-semibold"
-                >
-                  <ScrollText className="h-6 w-6" />
-                  <span className="">TLDR</span>
-                </Link>
-              </div>
+              <div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6"></div>
               <div className="flex-1">
                 <NavBar />
               </div>
             </div>
           </div>
           <div className="flex flex-col">
-            <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <header className="fixed left-0 right-0 top-0 z-10 flex h-14 items-center gap-4 bg-muted px-4 lg:h-[60px] lg:px-6">
+              <Link
+                href="/"
+                className="hidden items-center gap-2 font-semibold md:flex"
+              >
+                <ScrollText className="h-6 w-6" />
+                <span className="">TLDR</span>
+              </Link>
+
               <NavBarSheet />
               <Profile />
             </header>
 
-            <main>{children}</main>
+            <main className="mt-[61px]">{children}</main>
           </div>
         </div>
       </body>
