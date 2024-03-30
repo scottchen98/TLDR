38480cbd82cabb78f4f18619237be63255e8e492
scottchen98 <scott.chen1117@gmail.com>
2024-03-30 00:11:03 -0700

    create Greeting component

---
+++ src/app/greeting.tsx
@@ -0,0 +1,10 @@
+export default function Greeting() {
+  return (
+    <div className="relative top-[60%] flex flex-col gap-5 text-center text-4xl font-medium md:text-5xl lg:text-6xl">
+      <span className="mx-auto bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
+        Hello, Scott
+      </span>
+      <span className="text-[#c4c7c5]">Welcome to TLDR</span>
+    </div>
+  );
+}
