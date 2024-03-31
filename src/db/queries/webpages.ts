087a5e7774618cc5d743368976c88bdde262502a
scottchen98 <scott.chen1117@gmail.com>
2024-03-30 21:47:40 -0700

    create webpages queries

---
+++ src/db/queries/webpages.ts
@@ -0,0 +1,42 @@
+import { and, desc, eq, sql } from "drizzle-orm";
+import { db } from "..";
+import { webpages } from "../schema/webpages";
+
+export const baseQuery = db.select().from(webpages);
+
+export const getCurrentUserWebpages = baseQuery
+  .where(eq(webpages.userId, sql.placeholder("userId")))
+  .orderBy(desc(webpages.createdAt))
+  .prepare();
+
+export const getWebpage = baseQuery
+  .where(
+    and(
+      eq(webpages.userId, sql.placeholder("userId")),
+      eq(webpages.id, sql.placeholder("id")),
+    ),
+  )
+  .prepare();
+
+export const createWebpage = db
+  .insert(webpages)
+  .values({
+    userId: sql.placeholder("userId"),
+    url: sql.placeholder("url"),
+    title: sql.placeholder("title"),
+    summary: sql.placeholder("summary"),
+  })
+  .returning()
+  .prepare();
+
+export const deleteWebpage = db
+  .delete(webpages)
+  .where(
+    and(
+      eq(webpages.userId, sql.placeholder("userId")),
+      eq(webpages.id, sql.placeholder("id")),
+    ),
+  )
+  .prepare();
+
+export type Webpages = Awaited<ReturnType<typeof baseQuery.all>>[0];
