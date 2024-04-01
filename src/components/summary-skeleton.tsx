3d2552a05122310abb703b09c0cb48e1aae51fa3
scottchen98 <scott.chen1117@gmail.com>
2024-04-01 15:02:19 -0700

    update skeleton background color

--- src/components/summary-skeleton.tsx
+++ src/components/summary-skeleton.tsx
@@ -3,10 +3,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SummarySkeleton() {
   return (
     <div className="mt-10 space-y-5 p-8">
-      <Skeleton className="h-8 w-1/3" />
+      <Skeleton className="h-8 w-1/3 bg-[#D9ACF5]" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full bg-[#FFCEFE]" />
+        <Skeleton className="h-4 w-full bg-[#FFCEFE]" />
+        <Skeleton className="h-4 w-[70%] bg-[#FFCEFE]" />
       </div>
     </div>
   );
