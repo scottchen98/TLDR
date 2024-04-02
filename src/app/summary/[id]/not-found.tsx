25b2c6d702a355541b9d38a3f465a31152103785
scottchen98 <scott.chen1117@gmail.com>
2024-04-02 11:55:12 -0700

    add margin top

--- src/app/summary/[id]/not-found.tsx
+++ src/app/summary/[id]/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="mx-auto mt-8 flex flex-col items-center gap-4">
+    <div className="mx-auto mt-[103px] flex flex-col items-center gap-4">
       <h2 className="text-2xl">Summary Not Found</h2>
       <Link href="/" className="text-blue-500 hover:underline">
         Return Home
