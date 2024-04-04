c4ede9f45785b9c405186cdb2ce32041721e42ad
scottchen98 <scott.chen1117@gmail.com>
2024-04-04 09:22:13 -0700

    update max width

--- src/app/summary/layout.tsx
+++ src/app/summary/layout.tsx
@@ -2,7 +2,7 @@ import UrlQuery from "../url-query";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto max-w-6xl">
+    <div className="mx-auto max-w-5xl">
       <UrlQuery />
       {children}
     </div>
