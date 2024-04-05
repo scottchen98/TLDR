693427abc59d71c7159cf092878faf4a9c3eb739
scottchen98 <scott.chen1117@gmail.com>
2024-04-05 09:11:20 -0700

    Add authentication check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Plus, ScrollText } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import Link from "next/link";
 import NavBarSheet from "@/components/nav-bar-sheet";
@@ -18,16 +19,19 @@ export const metadata: Metadata = {
     "TLDR a web app that helps you quickly grasp the key points of any webpage. Simply paste a URL, and TLDR will extract the main content and provide you with a concise summary.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { isAuthenticated } = getKindeServerSession();
+  const isAuth = await isAuthenticated();
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="grid min-h-dvh w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-          <div className="hidden bg-muted/40 md:block">
+          <div className={`hidden bg-muted/40 ${isAuth ? "md:block" : ""}`}>
             <div className="flex h-full max-h-dvh flex-col gap-2 pt-2">
               <div className="mt-2 flex h-32 flex-col justify-center gap-12 px-4 md:px-2 lg:px-4">
                 <Link
@@ -54,7 +58,7 @@ export default function RootLayout({
               <Profile />
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className={`flex flex-col ${!isAuth ? "w-dvw" : ""}`}>
             <header className="pl-3 pt-3 md:p-0">
               <NavBarSheet />
             </header>
