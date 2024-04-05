9b3656813ff772f27b7a4171a68576755cf3649e
scottchen98 <scott.chen1117@gmail.com>
2024-04-05 09:11:58 -0700

    refactor profile component to display user information and add logout functionality

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,12 +1,25 @@
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-export default function Profile({ className }: { className?: string }) {
+export default async function Profile({ className }: { className?: string }) {
+  const { getUser } = getKindeServerSession();
+  const sessionUser = await getUser();
+
   return (
-    <LogoutLink className={cn("mx-auto mb-8 w-[80%]", className)}>
-      <Button className="w-full">Logout</Button>
-    </LogoutLink>
+    <>
+      {sessionUser && (
+        <div className={cn("mx-auto mb-8 w-[80%]", className)}>
+          <h2 className="mb-4 text-center text-xl font-medium">
+            Hello, <span>{sessionUser.given_name}</span>!
+          </h2>
+          <LogoutLink>
+            <Button className="w-full">Logout</Button>
+          </LogoutLink>
+        </div>
+      )}
+    </>
   );
 }
