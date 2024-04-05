5bd95a962494e877ee100698ff13c8c410cd2819
scottchen98 <scott.chen1117@gmail.com>
2024-04-04 22:34:38 -0700

    add errorMessage property to SummarizationStore

--- src/app/summarization-store.ts
+++ src/app/summarization-store.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 interface SummarizationStore {
   isSummarizing: boolean;
+  errorMessage: string;
   setIsSummarizing: (isSummarizing: boolean) => void;
+  setErrorMessage: (errorMessage: string) => void;
 }
 
 export const useSummarizationStore = create<SummarizationStore>((set) => ({
   isSummarizing: false,
+  errorMessage: "",
   setIsSummarizing: (isSummarizing) => set({ isSummarizing }),
+  setErrorMessage: (errorMessage) => set({ errorMessage }),
 }));
