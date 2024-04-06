b58a816c0242520764e4bbcce5f8a0b7aba135dd
scottchen98 <scott.chen1117@gmail.com>
2024-04-06 10:36:16 -0700

    refactor: allow vertical scrolling on the summary if needed

--- src/app/summary/layout.tsx
+++ src/app/summary/layout.tsx
@@ -2,9 +2,9 @@ import UrlQuery from "../url-query";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto max-w-5xl">
+    <div className="mx-auto flex max-h-dvh max-w-5xl flex-col">
       <UrlQuery />
-      {children}
+      <div className="grow overflow-y-auto">{children}</div>
     </div>
   );
 }
