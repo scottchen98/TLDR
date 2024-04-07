db8cedfc186a9600d5d0ffd5fd499d86a3c8f1bd
scottchen98 <scott.chen1117@gmail.com>
2024-04-06 22:20:11 -0700

    wrap UrlQuery component in a suspense boundary

--- src/app/summary/layout.tsx
+++ src/app/summary/layout.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import UrlQuery from "../url-query";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto flex max-h-dvh max-w-5xl flex-col">
-      <UrlQuery />
+      <Suspense>
+        <UrlQuery />
+      </Suspense>
       <div className="grow overflow-y-auto">{children}</div>
     </div>
   );
